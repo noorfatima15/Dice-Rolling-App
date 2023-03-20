dbb77805cd3dddaad90194e1cddb42e5e1d64d8c
noorfatima15 <noorfatima13689@gmail.com>
2023-03-20 21:47:46 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:dice_roller_app/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 5), () {});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.teal,
+        child: const Center(
+          child: Text('Dice Rolling App'),
+        ),
+      ),
+    );
+  }
+}
